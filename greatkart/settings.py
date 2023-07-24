f33fdd25a2cbd971bf09578b935c66c4e794a717
ProgramerSalar <manishkumar60708090@gmail.com>
2023-07-24 14:50:35 +0530

    create custom user

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'category',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'greatkart.wsgi.application'
 
+AUTH_USER_MODEL = 'accounts.Account'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
