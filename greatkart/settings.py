e5c3970013731f4dbfb7113b7654cedca95f3a2a
ProgramerSalar <manishkumar60708090@gmail.com>
2023-07-24 13:31:05 +0530

    static files are add in home.html

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'greatkart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    'greatkart/static',
+
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- greatkart/views.py
+++ greatkart/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse 
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("hello world")
+    return render(request, 'home.html')
+
+
+
+
