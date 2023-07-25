c99fd591736338b95ab0d61b0e9019ea607d7ab0
ProgramerSalar <manishkumar60708090@gmail.com>
2023-07-25 13:54:41 +0530

    how to use the category slug

--- store/views.py
+++ store/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, get_object_or_404
+from .models import Product
+from category.models import Category
 
 
 # Create your views here.
-def store(request):
-    return render(request, 'store/store.html')
+def store(request,category_slug=None):
+    categories = None 
+    product = None 
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
+
+
+
+    context = {
+        'products':products,
+        'product_count':product_count,
+    }
+    return render(request, 'store/store.html',context)
