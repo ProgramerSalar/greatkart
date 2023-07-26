406bb3ea787681ad1cbe320b4ed7569f1c5929db
ProgramerSalar <manishkumar60708090@gmail.com>
2023-07-26 22:40:33 +0530

    Implement data into Cart Page

--- carts/models.py
+++ carts/models.py
@@ -18,6 +18,11 @@ class CartItem(models.Model):
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
+    def sub_total(self):
+        return self.product.price * self.quantity
+    
+    
+
 
 
     def __str__(self):
--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from store.models import Product
 from .models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist  
+
+
+
+
+
+
+
+
+
+
 # Create your views here.
 
 
@@ -37,8 +48,8 @@ def add_cart(request, product_id):
             cart=cart,
         )
         cart_item.save()
-    return HttpResponse(cart_item.quantity)
-    exit()
+    # return HttpResponse(cart_item.product)
+    # exit()
 
     return redirect('cart')
 
@@ -51,5 +62,27 @@ def add_cart(request, product_id):
 
 
 
-def cart(request):
-    return render(request, 'store/cart.html')
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+
+    except ObjectDoesNotExist:
+        pass 
+
+    context = {
+        'total':total,
+        'quantity':quantity,
+        'cart_items':cart_items,
+
+    }
+
+    return render(request, 'store/cart.html',context)
+
+
+
+
+
