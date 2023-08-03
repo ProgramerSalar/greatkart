a291f4aff122602821c2ddb74e3d987312adfca4
ProgramerSalar <manishkumar60708090@gmail.com>
2023-08-03 13:29:58 +0530

    Modifiy Cart Counter and cart view the handle logged-in user

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -11,7 +11,11 @@ def counter(request):
     else:
         try:
             cart = Cart.objects.filter(cart_id=_cart_id(request))
-            cart_items = CartItem.objects.all().filter(cart=cart[:1])
+            if request.user.is_authenticated:  # user is assign 
+                cart_items = CartItem.objects.all().filter(user=request.user)  # all item are show in the cart 
+                
+            else:
+                cart_items = CartItem.objects.all().filter(cart=cart[:1])   # item are assing in cart 
             for cart_item in cart_items:
                 cart_count += cart_item.quantity
 
