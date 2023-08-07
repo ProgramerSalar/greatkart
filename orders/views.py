d4f2dff2fff8e8879d66e8b92ebdf1a1bb70162e
ProgramerSalar <manishkumar60708090@gmail.com>
2023-08-07 13:30:21 +0530

    end of payment system

--- orders/views.py
+++ orders/views.py
@@ -197,4 +197,32 @@ def place_order(request, total=0, quantity=0):
 
 
 def order_complete(request):
-    return render(request, 'orders/order_complete.html')
+    order_number = request.GET.get('order_number')
+    transID = request.GET.get('payment_id')
+
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_products = OrderProduct.objects.filter(order_id=order.id)
+        
+        payment = Payment.objects.get(payment_id=transID)
+
+        subtotal = 0
+        for i in ordered_products:
+            subtotal += i.product_price * i.quantity
+
+
+        context = {
+            'order':order,
+            'ordered_products':ordered_products,
+            'order_number':order.order_number,
+            'transID':payment.payment_id,
+            'payment':payment,
+            'subtotal':subtotal,
+        }
+        return render(request, 'orders/order_complete.html',context)
+    
+    except (Payment.DoesNotExist, Order.DoesNotExist):
+        return redirect('home')
+    
+
+
