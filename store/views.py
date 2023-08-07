9327f672ebd58c8d00dd050d584c2b2136d7ddc2
ProgramerSalar <manishkumar60708090@gmail.com>
2023-08-07 17:04:49 +0530

    Rating Average and Review Count Calculation

--- store/views.py
+++ store/views.py
@@ -56,11 +56,15 @@ def product_detail(request, category_slug, product_slug):
     except Exception as e:
         raise e
     
-    try:
-        orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+    if request.user.is_authenticated:
+        try:
+            orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
 
-    except OrderProduct.DoesNotExist:
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
+    else:
         orderproduct = None
+
 
 
 
