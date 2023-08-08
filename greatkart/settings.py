6e15840f87d9f101f37305bd4d4263b560e8e563
ProgramerSalar <manishkumar60708090@gmail.com>
2023-08-08 21:52:14 +0530

    Automatically logout after Inactivity

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
-from django.utils.translation import gettext_lazy
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,7 +54,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
+
+SESSION_EXPIRE_SECONDS = 3600  # 1 hour
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIMEOUT_REDIRECT = 'accounts/login'
+
 
 ROOT_URLCONF = 'greatkart.urls'
 
@@ -73,6 +80,9 @@ TEMPLATES = [
             ],
         },
     },
+
+
+
 ]
 
 WSGI_APPLICATION = 'greatkart.wsgi.application'
